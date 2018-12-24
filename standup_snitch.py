dabc92b8409209df786e71ddf4fba650b2b7b0ce
Neill <neillbogie@googlemail.com>
2018-12-24 18:24:02 +0000

    have only make_intro and make_conclusion write output
    add -l to log the raw json.  off by default.

--- standup_snitch.py
+++ standup_snitch.py
@@ -19,12 +19,14 @@ import json
 from collections import Counter
 import datetime as DT
 
+
 def format_channel_for_slack(channel_dict):
     return ''.join(['<#',
                     channel_dict['channel_id'],
                     '|',
                     channel_dict['channel_name'],
                     '>'])
+
 
 def format_user_for_slack(user_id, user_info):
     return ''.join(['<@',
@@ -33,34 +35,37 @@ def format_user_for_slack(user_id, user_info):
                     user_info['user_name'],
                     '>'])
 
+
 def format_user_for_text(user_id, user_info):
     return "%s (%s)" % (user_info['real_name'], user_info['user_name'])
 
+
 def json_pp(obj):
     return json.dumps(obj, indent=4, sort_keys=True)
-    
-def get_message_history(token, channel_id, channel_name, days):
+
+
+def get_message_history(token, channel_id, channel_name, days, should_log_raw_channel_history=False):
     ts = timestamp_for_days_ago(days)
-    print("Looking at history of channel %s (%s) for %d days.  Oldest timestamp requested: %s" % (channel_name, channel_id, days, ts))
     history_raw = slack_api.call_slack('channels.history',
                                        {'token': token,
                                         'channel': channel_id, 
                                         'oldest': ts,
-                                        'count': 1000}) # 1000 messages is the maximum allowed by the API.
-    with open('sensitive/channels.history.json', 'w') as f:
-        f.write(json_pp(history_raw))    
+                                        'count': 1000}) # 1000 messages is the maximum allowed by the API.    
+    if (should_log_raw_channel_history):
+        with open('sensitive/channels.history.json', 'w') as f:
+            f.write(json_pp(history_raw))    
     return [{'user': message['user'], 'ts': message['ts']}
             for message in history_raw['messages']
             if (message['type'] == 'message' and
                 'user' in message and
                 'ts' in message)]
 
+
 def aggregate_activity(history, users):
     user_activity_dict = {}
     for user_id in users:
         user_activity_dict[user_id] \
             = False
-
     for message in history:
         try:
             user = message['user']
@@ -70,19 +75,30 @@ def aggregate_activity(history, users):
             pass
     return user_activity_dict
 
-def make_introduction(input_channel, n_days):
-    return "Who's NOT present in the last %d days on #%s?" % ( n_days, input_channel['channel_name'] )
 
-def make_conclusion(active_users, users):
+def make_introduction(input_channel, n_days):
+    channel_name = input_channel['channel_name']
+    return "Slack report time: %s\n\nWho's NOT present in the last %d days on #%s?\n" % ( DT.datetime.now(), n_days, channel_name )
+
+
+def make_conclusion(active_users, counter, users):
+    res = []
     non_posters = [user_id for user_id in active_users
                    if active_users[user_id] == False]
-
     if len(non_posters) == 0:
-        return 'Go team!'
+        res.append('No-one is missing!  Go team!')
     else:
         tag_items = [format_user_for_text(user_id, users[user_id])
                      for user_id in non_posters]
-        return ', \n'.join(tag_items)
+        res.extend(tag_items)
+    res.append("\nMOST ACTIVE:")
+    for e in counter.most_common():
+         if e[0] in users:
+             res.append("%s: %s" % ( users[e[0]]['real_name'], e[1]))
+    res.append("")
+    res.append("")    
+    return "\n".join(res)
+
 
 def post_message(token, channel, text, bot_name):
     raise Exception("TRIED TO POST A MESSAGE TO SLACK!")
@@ -91,6 +107,7 @@ def post_message(token, channel, text, bot_name):
                           'channel': channel,
                           'text': text,
                           'username': bot_name})
+
 
 def parse_command_line():
     # Command line flags
@@ -101,7 +118,8 @@ def parse_command_line():
     parser.add_argument('-o', '--output_channel_file',
                         help = 'file with Slack channel to write to')
     parser.add_argument('-u', '--user_file', help = 'file with user list')
-    parser.add_argument('-d', '--num_days', action="store", default=10, type=int, help = 'number of days over which to look back')
+    parser.add_argument('-d', '--num_days', default=10, type=int, help = 'number of days over which to look back')
+    parser.add_argument('-l', '--log_raw_json', action='store_false', default=False, help = 'log raw json response to file: sensitive/channels.history.json')
     parser.add_argument('-r', '--dry_run', action = 'store_true',
                         help = 'flag to dry-run results to standard output')
     return parser.parse_args()
@@ -126,6 +144,7 @@ def read_config_files(args):
                                    'user_id': user['user_id']} for user in csv.DictReader(user_file)}
     return token, input_channel, output_channel, users                      
 
+
 def timestamp_for_days_ago(n_days):
     # no doubt there's a tidier way to do this.  Newbie code.
     now = DT.datetime.now()
@@ -133,20 +152,20 @@ def timestamp_for_days_ago(n_days):
     timestamp = (week_ago - DT.datetime(1970, 1, 1)) / DT.timedelta(seconds=1)
     return timestamp
 
+
 def run():
     args = parse_command_line()
     dry_run = args.dry_run
     n_days = int(args.num_days)
-
+    
     token, input_channel, output_channel, users = read_config_files(args)
-    
-    print("Slack report time: %s" % DT.datetime.now())
-    
+
     # Slack API call to get history
     message_history = get_message_history(token,
                                           input_channel['channel_id'],
                                           input_channel['channel_name'],
-                                          n_days)
+                                          n_days,
+                                          args.log_raw_json)
     
     #calc who is active
     active_users = aggregate_activity(message_history, users)
@@ -154,20 +173,12 @@ def run():
     # Preamble
     introduction = make_introduction(input_channel, n_days)
 
-
     counter = Counter(map(lambda x: x['user'], message_history))
     
-    print("\nMOST ACTIVE:")
-    for e in counter.most_common():
-         if e[0] in users:
-             print("%s: %s" % ( users[e[0]]['real_name'], e[1]))
-    print("\n\n")
-    
-    # Call out non-posters or congratulate the team
-    conclusion = make_conclusion(active_users, users)
+    conclusion = make_conclusion(active_users, counter, users)
 
     # Assemble the full_message
-    full_message = '\n'.join([introduction, conclusion])
+    full_message = '\n'.join(['```', introduction, conclusion, '```'])
 
     # Slack API call to publish summary
     if dry_run:
